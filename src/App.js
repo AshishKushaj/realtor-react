1dfa28e3bd316a7a52677e48be61d72167af9faa
Ashish Kushaj <ashishkushajak@gmail.com>
2023-07-17 03:22:34 +0530

    added header and used useLocation and navigation

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Offers from './pages/Offers';
 import ForgotPassword from './pages/ForgotPassword';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
       <Router>
+
+        <Header/>
+
         <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/profile" element={<Profile/>} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useLocation, Navigate, useNavigate } from 'react-router-dom';
+
+export default function Header() {
+
+  const navigate=useNavigate();
+  const location = useLocation();
+
+  function onRoute(route) {
+    return route === location.pathname;
+  }
+
+  return (
+    <div className='sticky top-0 z-50 bg-white border-b shadow-sm'>
+      <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
+        <div>
+          <img src='https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg' alt='logo' className='h-5 cursor-pointer' onClick={()=> navigate("/")} />
+        </div>
+
+        <div>
+          <ul className='flex space-x-10'>
+
+            <li  onClick={()=>navigate("/")} 
+              className={`cursor-pointer py-3 text-sm font-semibold text-black  border-b-[3px] ${onRoute("/") ? 'border-b-red-500  text-black' : 'border-b-transparent text-gray-400'}`}>Home</li>
+
+            <li onClick={()=>navigate("/offers")} 
+              className={`cursor-pointer py-3 text-sm font-semibold text-black border-b-[3px]  ${onRoute("/offers") ? 'border-b-red-500 text-black ' : 'border-b-transparent text-gray-400'}`}>Offers</li>
+
+            <li onClick={()=>navigate("/sign-in")} 
+              className={`cursor-pointer py-3 text-sm font-semibold text-black border-b-[3px]  ${onRoute("/sign-in") ? 'border-b-red-500 text-black' : 'border-b-transparent text-gray-400'}`}>Sign In</li>
+
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
