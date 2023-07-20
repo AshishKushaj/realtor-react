4c04b0ee3490b5298cd2bcc683eabed995a12e81
Ashish Kushaj <ashishkushajak@gmail.com>
2023-07-21 02:58:02 +0530

    made header dynamic for signIn and profile part

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation,  useNavigate } from 'react-router-dom';
+import { useAuthStatus } from '../hooks/useAuthStatus';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 export default function Header() {
 
   const navigate=useNavigate();
   const location = useLocation();
+  const auth= getAuth()
+  const [pageState,setPageState]=useState("Sign In")
+
+  useEffect(()=>{
+    onAuthStateChanged(auth,(user)=>{
+      if(user){
+        setPageState("Profile")
+      }else{
+        setPageState("Sign In")
+      }
+    })
+  },[auth])
 
   function onRoute(route) {
     return route === location.pathname;
   }
+
+  const loggedIn=useAuthStatus()
 
   return (
     <div className='sticky top-0 z-50 bg-white border-b shadow-sm'>
@@ -21,13 +37,13 @@ export default function Header() {
           <ul className='flex space-x-10'>
 
             <li  onClick={()=>navigate("/")} 
-              className={`cursor-pointer py-3 text-sm font-semibold text-black  border-b-[3px] ${onRoute("/") ? 'border-b-red-500  text-black' : 'border-b-transparent text-gray-400'}`}>Home</li>
+              className={`cursor-pointer py-3 text-sm font-semibold text-black  border-b-[3px] ${onRoute("/") ? 'border-b-red-500 transition ease-in-out duration-500 text-black' : 'border-b-transparent text-gray-400'}`}>Home</li>
 
             <li onClick={()=>navigate("/offers")} 
-              className={`cursor-pointer py-3 text-sm font-semibold text-black border-b-[3px]  ${onRoute("/offers") ? 'border-b-red-500 text-black ' : 'border-b-transparent text-gray-400'}`}>Offers</li>
+              className={`cursor-pointer py-3 text-sm font-semibold text-black border-b-[3px]  ${onRoute("/offers") ? 'border-b-red-500 transition ease-in-out duration-500 text-black ' : 'border-b-transparent text-gray-400'}`}>Offers</li>
 
-            <li onClick={()=>navigate("/sign-in")} 
-              className={`cursor-pointer py-3 text-sm font-semibold text-black border-b-[3px]  ${onRoute("/sign-in") ? 'border-b-red-500 text-black' : 'border-b-transparent text-gray-400'}`}>Sign In</li>
+            <li onClick={()=>navigate("/profile")} 
+              className={`cursor-pointer transition ease-in-out duration-500 py-3 text-sm font-semibold text-black border-b-[3px]  ${(onRoute("/sign-in") || onRoute("/profile")) ? 'border-b-red-500 text-black' : 'border-b-transparent text-gray-400'}`}> {pageState} </li>
 
           </ul>
         </div>
