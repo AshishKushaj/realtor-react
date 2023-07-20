7bdc39193eb8f5fb2a9884b38076cf4338486504
Ashish Kushaj <ashishkushajak@gmail.com>
2023-07-21 03:34:00 +0530

    added a spinner as a loading screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
   const loggedIn=useAuthStatus()
 
   return (
-    <div className='sticky top-0 z-50 bg-white border-b shadow-sm'>
+    <div className='sticky top-0 z-40 bg-white border-b shadow-sm'>
       <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
         <div>
           <img src='https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg' alt='logo' className='h-5 cursor-pointer' onClick={()=> navigate("/")} />
--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom';
 import {useAuthStatus} from '../hooks/useAuthStatus';
+import Spinner from './Spinner';
 
 export default function PrivateRoute() {
     const {loggedIn,loading}= useAuthStatus();
 
     if(loading){
-        return <h1>Loading...</h1> 
+        return <Spinner/> 
     }
 
   return loggedIn? <Outlet/> : <Navigate to="/sign-in"/>
