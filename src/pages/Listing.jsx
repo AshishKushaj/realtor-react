3674d85e5eccf8d3c5d58338678a7bc71d4165f7
Ashish Kushaj <ashishkushajak@gmail.com>
2023-07-24 23:18:25 +0530

    added share link functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import { db } from "../firebase";
 import Spinner from "../components/Spinner";
 import { Swiper, SwiperSlide } from "swiper/react";
+import {FaShare} from 'react-icons/fa'
 import  {
   EffectFade,
   Autoplay,
@@ -13,11 +14,12 @@ import  {
 import SwiperCore from 'swiper';
 import "swiper/css/bundle";
 
+
 export default function Listing() {
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
-
+  const [shareLinkCopied,setShareLinkCopied]=useState(false)
 
   SwiperCore.use([Autoplay, Navigation, Pagination]);
 
@@ -34,12 +36,13 @@ export default function Listing() {
     setLoading(false);
   }, [params.listingId]);
 
+
   if (loading || !listing) {
     return <Spinner />;
   }
 
   return (
-    <main>
+    <main className="">
       <Swiper
         slidesPerView={1}
         navigation
@@ -54,12 +57,25 @@ export default function Listing() {
               className="relative w-full overflow-hidden h-[300px]"
               style={{
                 background: `url(${listing.imgUrls[index]}) center no-repeat`,
-                backgroundSize:'cover' 
+                backgroundSize: "cover",
               }}
             ></div>
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        className=" fixed right-[3%] top-[13%] z-10 bg-white cursor-pointer w-12 h-12 rounded-full border-gray-600 border-2 flex justify-center items-center  "
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, [2000]);
+        }}
+      >
+        <FaShare className="text-lg "></FaShare>
+      </div>
+      {shareLinkCopied && <p className=" fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-lg bg-white z-10 p-2 ">Link Copied!!</p>}
     </main>
   );
 }
