47e6e46908023a97473e0596e4a0c8fc5a71aba6
Ashish Kushaj <ashishkushajak@gmail.com>
2025-08-06 03:46:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import SignUp from './pages/SignUp'; 
 import Offers from './pages/Offers';
 import ForgotPassword from './pages/ForgotPassword';
 import Header from './components/Header';
